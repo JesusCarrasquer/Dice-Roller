4c61d248ca85b2afcd5cb9497380f3735f5339e9
Jesús Carrasquer <jesuscarrasquer@gmail.com>
2023-08-08 20:14:16 +0200

    Add 5e spell selection

--- Modules/SpellRoller.py
+++ Modules/SpellRoller.py
@@ -2,6 +2,8 @@ import requests
 import json
 
 def startModule():
+
+    # SELECCIÓN DE CLASE
     print('Cargando clases...')
     classesCall = requests.get('https://www.dnd5eapi.co/api/classes').json()['results']
     spellcastingClasses = []
@@ -10,7 +12,38 @@ def startModule():
         detailsCall = requests.get('https://www.dnd5eapi.co/api/classes/'+className+'/Spellcasting').status_code
         if detailsCall == 200:
             spellcastingClasses.append(className)
-    print('¿De que clase es el hechizo?')
     for i in range (0,len(spellcastingClasses)):
         print(i,'-',spellcastingClasses[i])
+    selectedClassInt = input('¿De que clase es el hechizo?\n')
+    validated = False
+    while not validated:
+        try:
+            selectedClassInt = int(selectedClassInt)
+            if selectedClassInt < 0 or selectedClassInt >= len(spellcastingClasses):
+                raise ValueError()
+            validated = True
+        except ValueError:
+            selectedClassInt = input('Error, debes introducir un numero correcto y dentro del rango presentado')
+    selectedClass = spellcastingClasses[selectedClassInt]
+    
+    # SELECCIÓN DE NIVEL
+    print('Cargando hechizos...')
+    classSpells = requests.get('https://www.dnd5eapi.co/api/classes/'+selectedClass+'/spells').json()['results']
+    for spell in range(0,len(classSpells)):
+        print(str(spell) + ' - ' + classSpells[spell]['name'])
+        
+    selectedLevelInt = input('¿Que hechizo quieres tirar?\n ')
+    validated = False
+    while not validated:
+        try:
+            selectedLevelInt = int(selectedLevelInt)
+            if selectedLevelInt < 0 or selectedLevelInt >= len(classSpells):
+                raise ValueError()
+            validated = True
+        except ValueError:
+            selectedLevelInt = input('Error, debes introducir un numero correcto y dentro del rango presentado')
+    
+
+
+
     input('Pulsa cualquier boton para continuar...')
