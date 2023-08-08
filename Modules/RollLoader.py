b2f8afd20ed60cbba7342d3371aee64e6bace4eb
Jesús Carrasquer <jesuscarrasquer@gmail.com>
2023-08-08 12:52:07 +0200

    Fix empty rolls loading

--- Modules/RollLoader.py
+++ Modules/RollLoader.py
@@ -2,8 +2,11 @@ import Utilities.SystemUtilities as ut
 import Utilities.DiceRoller as dr
 
 def startModule():
-    print('¿Que tirada quieres cargar?')
     savedRolls = ut.listSaved()
+    if len(savedRolls)==0:
+        input('No se ha encontrado ninguna tirada guardada, presiona enter para continuar...')
+        return
+    print('¿Que tirada quieres cargar?')
     for i in range (0,len(savedRolls)):
         print(i,'-',savedRolls[i][:-4])
     selectedRoll = input('Introduce aqui tu selección:')
